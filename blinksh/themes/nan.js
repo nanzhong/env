30d94e4fb03573e9a297b3d9eada5df1d9dfe841
Nan Zhong <nzhong@digitalocean.com>
2018-08-07 22:25:06 -0400

    add blinksh nan theme

---
+++ blinksh/themes/nan.js
@@ -0,0 +1,26 @@
+black       = '#000000';
+red         = '#ff005f'; // red
+green       = '#5fd75f'; // green
+yellow      = '#ffd787'; // yellow
+blue        = '#87afff'; // blue
+magenta     = '#ff87d7'; // pink
+cyan        = '#5fffd7'; // cyan
+white       = '#d0d0d0'; // light gray
+lightBlack  = '#5f5f5f'; // medium gray
+lightRed    = '#ff0087'; // red
+lightGreen  = '#87d787'; // green
+lightYellow = '#ffd7af'; // yellow
+lightBlue   = '#afafff'; // blue
+lightMagenta= '#ffafd7'; // pink
+lightCyan   = '#87ffd7'; // cyan
+lightWhite  = '#ffffff'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', '#eeeeee');
+t.prefs_.set('background-color', '#262626');
