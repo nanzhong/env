e02de8e7c135111d7ee758b1d55ed2a7d262e475
Nan Zhong <nan@notanumber.io>
2018-09-05 22:31:56 -0400

    update fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,25 +1,45 @@
 function prompt::colour::fst; set_color -o 6af; end
 function prompt::colour::snd; set_color -o cf3; end
-function prompt::colour::trd; set_color -o fff; end
+function prompt::colour::trd; set_color -o aaa; end
 function prompt::colour::dim; set_color -o 888; end
 function prompt::colour::err; set_color -o f30; end
 function prompt::colour::off; set_color normal; end
 
-function fish_prompt
-  set -l code $status
-  set -l base (basename "$PWD")
+function git_is_repo -d "Check if directory is a repository"
+  test -d .git; or command git rev-parse --git-dir >/dev/null ^/dev/null
+end
 
-  function prompt::status::colour -S
-    test $code -ne 0; and prompt::colour::err; or prompt::colour::snd
-  end
+function git_branch_name -d "Get current branch name"
+  command git symbolic-ref --short HEAD
+end
+
+function git_is_touched -d "Check if repo has any changes"
+  test -n (echo (command git status --porcelain))
+end
+
+function fish_prompt
+  set -l base (basename "$PWD")
 
   if test "$PWD" != "/"
     prompt_pwd | sed "s|$base|"(prompt::colour::trd)"$base"(prompt::colour::off)"|g" \
     | sed "s|~|"(prompt::colour::fst)"~"(prompt::colour::off)"|g" \
-    | sed "s|/|"(prompt::status::colour)"/"(prompt::colour::off)(prompt::colour::dim)"|g" \
+    | sed "s|/|"(prompt::colour::snd)"/"(prompt::colour::off)(prompt::colour::dim)"|g" \
     | tr -d \n
   else
     printf (prompt::colour::fst)"/"(prompt::colour::off)
   end
-  printf (prompt::status::colour)" ≡ "(prompt::colour::off)
+  printf (prompt::colour::off)" "
+end
+
+function fish_right_prompt
+  set -l code $status
+
+  function prompt::status::colour -S
+    test $code -ne 0; and prompt::colour::err; or prompt::colour::snd
+  end
+  if git_is_repo
+    echo (prompt::status::colour)" ≡ "(prompt::colour::dim)(git_branch_name)(prompt::colour::off)
+  else
+    echo (prompt::status::colour)" ≡"(prompt::colour::off)
+  end
 end
