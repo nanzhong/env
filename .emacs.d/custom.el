c59dfb0e84894f64efe822668714b270ce7aeb12
Nan Zhong <nan@notanumber.io>
2018-09-09 15:07:38 -0400

    update emacs config

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -13,12 +13,10 @@
  '(comint-scroll-show-maximum-output t)
  '(comint-scroll-to-bottom-on-input t)
  '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(package-selected-packages
-   (quote
-    (magit-todos which-key diff-hl highlight-indent-guides dashboard org-jira yasnippet-snippets yasnippet esh-autosuggest company-quickhelp zerodark-theme yaml-mode xterm-color web-mode vue-mode use-package unicode-fonts telephone-line swift-mode smartparens shackle rspec-mode robe rinari rbenv rainbow-mode rainbow-delimiters protobuf-mode projectile-rails perspeen persp-projectile nvm multi-term mu4e-alert moody minions magithub js2-mode ibuffer-vc ibuffer-projectile haml-mode go-projectile gnus-desktop-notify git-gutter-fringe fontify-face flycheck-swift flx fish-mode eyebrowse exec-path-from-shell enh-ruby-mode editorconfig dtrt-indent direnv counsel-projectile company-sourcekit company-lua company-go company-box apib-mode ace-window)))
- '(tramp-syntax (quote default) nil (tramp)))
+   '(magit-todos which-key diff-hl highlight-indent-guides dashboard org-jira yasnippet-snippets yasnippet esh-autosuggest company-quickhelp zerodark-theme yaml-mode xterm-color web-mode vue-mode use-package unicode-fonts telephone-line swift-mode smartparens shackle rspec-mode robe rinari rbenv rainbow-mode rainbow-delimiters protobuf-mode projectile-rails perspeen persp-projectile nvm multi-term mu4e-alert moody minions magithub js2-mode ibuffer-vc ibuffer-projectile haml-mode go-projectile gnus-desktop-notify git-gutter-fringe fontify-face flycheck-swift flx fish-mode eyebrowse exec-path-from-shell enh-ruby-mode editorconfig dtrt-indent direnv counsel-projectile company-sourcekit company-lua company-go company-box apib-mode ace-window))
+ '(tramp-syntax 'default nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
