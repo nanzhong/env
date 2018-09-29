c5af321447183971debc50e4b36afeb1596d8de8
Nan Zhong <nan@notanumber.io>
2018-09-29 16:44:49 -0400

    Cleanup dotfiles

--- .offlineimap.py
+++
@@ -1,9 +0,0 @@
-import json
-import subprocess
-
-def secure_string_for(account, service, value):
-    return json.loads(subprocess.check_output(["security",
-                                               "find-generic-password",
-                                               "-a", account,
-                                               "-s", service,
-                                               "-w"]).strip())[value]
