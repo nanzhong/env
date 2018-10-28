44d5db46b53b2d9ab010741835ec323288f9c17f
Nan Zhong <nan@notanumber.io>
2018-10-27 21:23:42 -0400

    [emacs] Improve org config

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -13,6 +13,7 @@
 (global-set-key (kbd "C-c b") 'org-switchb)
 
 (setq org-agenda-files (list (concat org-directory "/notes.org")
+                             (concat org-directory "/eva.org")
                              (concat org-directory "/todo.org")
                              (concat org-directory "/cal/do.org")
                              (concat org-directory "/cal/nan.org")
@@ -22,6 +23,42 @@
                                  (org-agenda-files :maxlevel . 9))))
 
 (add-hook 'org-mode-hook 'org-indent-mode)
+
+(setq org-capture-templates
+      `(("t" "Task templates")
+        ("tp" "Personal task"
+         entry (file+headline ,(concat org-directory "/todo.org") "Personal")
+         "* %?")
+        ("td" "DigitalOcean task"
+         entry (file+headline ,(concat org-directory "/todo.org") "DigitalOcean")
+         "* %?")
+
+        ("n" "Note"
+         entry (file ,(concat org-directory "/notes.org"))
+         "* %?\n%U")
+
+        ("e" "Eva related templates")
+        ("et" "Eva tracking"
+         entry (file+headline ,(concat org-directory "/eva.org") "Tracking")
+         "* %?\n:PROPERTIES:\n:Application: %^{Application|Google Doc|OneNote|n/a}\n:END:\n%T"
+         :empty-lines 1)))
+
+;; Re-define org-switch-to-buffer-other-window to NOT use org-no-popups.
+;; Primarily for compatibility with shackle.
+(defun org-switch-to-buffer-other-window (args)
+  "Switch to buffer in a second window on the current frame.
+In particular, do not allow pop-up frames.
+Returns the newly created buffer.
+Redefined to allow pop-up windows."
+  ;;  (org-no-popups
+  ;;     (apply 'switch-to-buffer-other-window args)))
+  (switch-to-buffer-other-window args))
+
+(use-package org-agenda-property
+  :ensure t
+  :commands org-agenda
+  :config
+  (setq org-agenda-property-list '("LOCATION" "Application")))
 
 ;; (use-package org-jira
 ;;   :ensure t
