e4b77f4b5aecd209092b1836ef623e56821fe667
Nan Zhong <nan@notanumber.io>
2018-11-20 13:33:28 -0500

    Use smarter logic for bootstrapping container

--- session-init.sh
+++ session-init.sh
@@ -5,15 +5,29 @@ set -e
 echo "Attach to existing tmux session if it exists..."
 /usr/bin/tmux attach || true
 
-eval $(~/bin/op signin my.1password.com nan@notanumber.io)
+echo "Checking for host volume..."
+if [ ! -d ~/host ]; then
+    echo "~/host not attached!"
+    exit 1
+fi
 
-echo "Setting up ssh key..."
-mkdir -p ~/.ssh
-~/bin/op get item "key.workstation.do" | jq -r .details.notesPlain > ~/.ssh/id_ed25519
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_ed25519
-ssh-keygen -yf ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
-chmod 644 ~/.ssh/id_ed25519.pub
+echo "Checking ssh keys..."
+if [ -d ~/host/.ssh ]; then
+    cp -a ~/host/.ssh ~/.ssh
+else
+    eval $(~/bin/op signin my.1password.com nan@notanumber.io)
+    echo "Setting up ssh key..."
+    mkdir -p ~/.ssh
+    ~/bin/op get item "key.workstation.do" | jq -r .details.notesPlain > ~/.ssh/id_ed25519
+    chmod 700 ~/.ssh
+    chmod 600 ~/.ssh/id_ed25519
+    ssh-keygen -yf ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+    chmod 644 ~/.ssh/id_ed25519.pub
+fi
+
+if [ -d ~/host/src/org ]; then
+    ln -s ~/host/src/org ~/org
+fi
 
 echo "Starting tmux session..."
 /usr/bin/tmux new
