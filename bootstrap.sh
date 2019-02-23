fb0e682adb9f73f12619d80828f4b452c81262e6
Nan Zhong <nan@notanumber.io>
2019-02-23 17:17:05 -0500

    Bump kubectl and minikube on host boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,8 +36,8 @@ apt-get update && \
             dnsutils
 
 echo "Fetching kubernetes dependencies..."
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.2/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /usr/local/bin/kubectl
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.33.1/minikube-linux-amd64 && chmod +x minikube && mv minikube /usr/local/bin/
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.3/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /usr/local/bin/kubectl
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.34.1/minikube-linux-amd64 && chmod +x minikube && mv minikube /usr/local/bin/
 curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && install docker-machine-driver-kvm2 /usr/local/bin/ && rm docker-machine-driver-kvm2
 
 echo "Configure custom ip block for docker..."
