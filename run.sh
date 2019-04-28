b418c290d3ea532b9dcfe8bb83ee4e9f40440cf0
Nan Zhong <nan@notanumber.io>
2019-04-28 11:02:47 -0400

    Mount src directory to the same place in container

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ docker run -it \
        -e TZ=America/Toronto \
        -e KEYBASE_USERNAME=nan \
        -v /:/root/host \
+       -v /root/src:/root/src \
        -v /var/run/docker.sock:/var/run/docker.sock \
        --detach-keys "ctrl-^,d" \
        --net host \
