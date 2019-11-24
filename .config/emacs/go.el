4336bd9d33c25a14a0a9902feed5c2ae2bf5393c
Nan Zhong <nan@notanumber.io>
2019-11-24 18:02:30 +0000

    [emacs] Use lsp formatting for go before save hook

--- .config/emacs/go.el
+++ .config/emacs/go.el
@@ -10,8 +10,6 @@
   :bind (:map go-mode-map
               ("C-c C-c" . compile))
   :config
-  (setq gofmt-command "goimports")
-  (setq gofmt-args '("-local" "do"))
   (add-hook 'go-mode-hook (lambda ()
                             (setq-local compile-command "go build -v; and go test -v; and go vet")
                             (setq-local compilation-read-command nil)
@@ -23,7 +21,8 @@
                                            (slot          . 0)
                                            (side          . bottom)
                                            (window-height . 0.35)))))
-  (add-hook 'before-save-hook #'gofmt-before-save))
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
 
 (use-package go-projectile
   :ensure t
