65e4018ca65d041bcc2ae6e5a132a42c22a4cd93
Nan Zhong <nan@notanumber.io>
2020-01-05 16:19:41 -0500

    [tmux] Fix icons

--- .config/fish/functions/tmux-status-ip.fish
+++ .config/fish/functions/tmux-status-ip.fish
@@ -1,4 +1,4 @@
 function tmux-status-ip
     set ip (ip addr show eth0 | grep -Po 'inet \K[\d.]+')
-    echo "酪 $ip"
+    echo " $ip"
 end
--- .config/fish/functions/tmux-status-vpn.fish
+++ .config/fish/functions/tmux-status-vpn.fish
@@ -1,12 +1,12 @@
 function tmux-status-vpn
     set addr (ip addr show gpd0 2> /dev/null)
     if echo $addr | grep -q "state DOWN"
-        echo "旅 ..."
+        echo " ..."
         return 1
     end
     set ip (echo $addr | grep -Po 'inet \K[\d.]+')
     if test -z $ip
         set ip ...
     end
-    echo "旅 $ip"
+    echo " $ip"
 end
