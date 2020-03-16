e0210d1fdea55f9a829a03261178adb117808bd7
Nan Zhong <me@nanzho.ng>
2020-03-16 17:40:57 -0400

    [emacs] Enable web mode for ts(x) files

--- .config/emacs/web.el
+++ .config/emacs/web.el
@@ -17,6 +17,7 @@
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts[x]\\'" . web-mode)))
 
 ;;; web.el ends here
