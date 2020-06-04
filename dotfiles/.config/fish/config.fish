7e6040497858bab49538e421869041e940cf2594
Nan Zhong <me@nanzho.ng>
2020-06-04 11:52:13 -0400

    [fish] remove rbenv and n path config

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -11,10 +11,5 @@ eval (direnv hook fish)
 if not set -q GOPATH
     set -gx GOPATH $HOME/go
 end
-if not set -q N_PREFIX
-    set -gx N_PREFIX $HOME/.n
-end
 
-set -U fish_user_paths $HOME/bin $HOME/.fzf/bin $HOME/.rbenv/bin $GOPATH/bin $N_PREFIX/bin /usr/local/bin
-
-status --is-interactive; and source (rbenv init -|psub)
+set -U fish_user_paths $HOME/bin $GOPATH/bin
