3672359f10ff16869d1da78696b69c7372ee324f
Nan Zhong <nzhong@digitalocean.com>
2020-06-22 08:16:26 -0400

    [nix] Add default nameservers for wrk

--- nix/machines/wrk.nix
+++ nix/machines/wrk.nix
@@ -12,10 +12,14 @@ in {
     ip=$(ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
     subnet=$(ip route | grep -Po '^\d+(.\d+){3}/\d+(?= dev eth0)')
     gateway=$(ip route | grep -Po '(?<=default via )[.\d]+')
+    ip rule delete from $ip table 128 || true
     ip rule add from $ip table 128
     ip route add table 128 to $subnet dev eth0
     ip route add table 128 default via $gateway
   '';
+
+  networking.nameservers = [ "10.30.0.7" "10.30.0.8" ];
+  networking.search = [ "internal.digitalocean.com" "consul" ];
 
   users.users = {
     nzhong = {
