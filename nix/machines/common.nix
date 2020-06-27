85e9b84927d30ac746eb7e6ecaa9f7b32d03f4ae
Nan Zhong <me@nanzho.ng>
2020-06-27 09:55:49 -0400

    [nix] Enable ip forward in dev

--- nix/machines/common.nix
+++ nix/machines/common.nix
@@ -8,15 +8,16 @@ in {
     cleanTmpDir = true;
     kernel.sysctl = {
       "fs.inotify.max_user_watches" = "1048576";
+      "net.ipv4.ip_forward" = "1";
     };
   };
 
   security.sudo.wheelNeedsPassword = false;
-  
+
   networking.firewall = {
     allowPing = true;
   };
-  
+
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [
     (import ../overlays/mosh.nix)
@@ -37,18 +38,18 @@ in {
     tree
     wget
   ];
-  
+
   programs = {
     fish.enable = true;
     mosh.enable = true;
   };
-  
+
   services.openssh.enable = true;
-  
+
   virtualisation.docker.enable = true;
-  
+
   users.mutableUsers = false;
-  
+
   users.users.root = {
     openssh.authorizedKeys.keys = keys.sshPub;
   };
