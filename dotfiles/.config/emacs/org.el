4ba82fdd2baa898a359451934820eacb64422180
Nan Zhong <me@nanzho.ng>
2020-09-27 09:38:38 -0400

    [emacs] Refresh org config

--- dotfiles/.config/emacs/org.el
+++ dotfiles/.config/emacs/org.el
@@ -1,50 +1,71 @@
+;;; -*- lexical-binding: t; -*-
 ;;; org.el --- org specific
 
 ;;; Commentary:
 
 ;;; Code:
 
-(setq org-directory "~/org")
-(setq org-default-notes-file "~/org/nan.org")
+(use-package org
+  :bind (("C-c c" . org-capture)
+         ("C-c a" . org-agenda)
+         ("C-c b" . org-switchb)
+         ("C-c l" . org-link))
+  :init
+  (add-hook 'org-mode-hook 'visual-line-mode)
+  :config
+  (defun org-find-heading-in-datetree (heading)
+    (lambda ()
+      (org-datetree-find-date-create (calendar-current-date))
+      (goto-char (point-at-eol))
+      (when (not (re-search-forward
+                  (format org-complex-heading-regexp-format
+                          (regexp-quote heading)) nil t))
+        (insert (concat "\n**** " heading "\n")))
+      (goto-char (point-at-eol))))
+  (setq org-directory "~/org"
+        org-default-notes-file "~/org/nan.org"
+        org-agenda-files (list (concat org-directory "/nan.org")
+                               (concat org-directory "/do.org"))
+        org-refile-targets (quote ((nil :maxlevel . 9)
+                                   (org-agenda-files :maxlevel . 9)))
+        org-todo-keywords '((sequence "TODO(t)" "BLOCKED(b@/!)" "|" "DONE(d!)" "REJECTED(r@)"))
+        org-log-into-drawer "LOGBOOK"
+        org-startup-indented t
+        org-capture-templates
+        `(("h" "Health")
+          ("he" "Weight"
+           plain (file+function ,(concat org-directory "/health.org") ,(org-find-heading-in-datetree "Weight"))
+           ":PROPERTIES:\n:VALUE: %^{Weight (kg)}\n:END:\n%?"
+           :jump-to-captured t)
+          ("hf" "Food Log"
+           entry (file+function ,(concat org-directory "/health.org") ,(org-find-heading-in-datetree "Food"))
+           "* %?"
+           :jump-to-captured t)
+          ("t" "Tasks")
+          ("tp" "Personal Task"
+           entry (file+headline ,(concat org-directory "/nan.org") "Personal")
+           "* TODO %?")
+          ("td" "DigitalOcean Task"
+           entry (file+headline ,(concat org-directory "/do.org") "DigitalOcean")
+           "* TODO %?")
 
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c b") 'org-switchb)
-
-(setq org-agenda-files (list( concat org-directory "/nan.org")
-                             (concat org-directory "/do.org")))
-
-(setq org-refile-targets (quote ((nil :maxlevel . 9)
-                                 (org-agenda-files :maxlevel . 9))))
-
-(add-hook 'org-mode-hook 'org-indent-mode)
-
-(setq org-capture-templates
-      `(("t" "Task templates")
-        ("tp" "Personal task"
-         entry (file+headline ,(concat org-directory "/nan.org") "Personal")
-         "* %?")
-        ("td" "DigitalOcean task"
-         entry (file+headline ,(concat org-directory "/do.org") "DigitalOcean")
-         "* %?")))
-
-;; Re-define org-switch-to-buffer-other-window to NOT use org-no-popups.
-;; Primarily for compatibility with shackle.
-(defun org-switch-to-buffer-other-window (args)
-  "Switch to buffer in a second window on the current frame.
-In particular, do not allow pop-up frames.
-Returns the newly created buffer.
-Redefined to allow pop-up windows."
-  ;;  (org-no-popups
-  ;;     (apply 'switch-to-buffer-other-window args)))
-  (switch-to-buffer-other-window args))
+          ("r" "Review")
+          ("rn" "Weekly review - Personal"
+           entry (file+olp+datetree ,(concat org-directory "/nan-review.org"))
+           "* %?"
+           :treetype week
+           :jump-to-captured t)
+          ("rd" "Weekly Review - DigitalOcean"
+           entry (file+olp+datetree ,(concat org-directory "/do-review.org"))
+           "* %?"
+           :treetype week
+           :jump-to-captured t))))
 
 (use-package org-agenda-property
   :ensure t
   :commands org-agenda
   :config
-  (setq org-agenda-property-list '("LOCATION" "Application")))
+  (setq org-agenda-property-list '("LOCATION")))
 
 ;; (use-package org-jira
 ;;   :ensure t
