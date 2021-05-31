0042d245b449dfe8da2820a81a88bced05623557
Nan Zhong <nzhong@digitalocean.com>
2021-05-31 15:13:57 -0400

    [emacs] Remove org-roam-server
    org-roam-server org-roam v2 and I don't really use it often.

--- dotfiles/.config/emacs/org.el
+++ dotfiles/.config/emacs/org.el
@@ -89,20 +89,20 @@
               (("C-c r i" . org-roam-insert))
               (("C-c r I" . org-roam-insert-immediate))))
 
-(use-package org-roam-server
-  :straight t
-  :config
-  (setq org-roam-server-host "127.0.0.1"
-        org-roam-server-port 9999
-        org-roam-server-authenticate nil
-        org-roam-server-export-inline-images t
-        org-roam-server-serve-files nil
-        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-        org-roam-server-network-poll t
-        org-roam-server-network-arrows nil
-        org-roam-server-network-label-truncate t
-        org-roam-server-network-label-truncate-length 60
-        org-roam-server-network-label-wrap-length 20))
+;; (use-package org-roam-server
+;;   :straight t
+;;   :config
+;;   (setq org-roam-server-host "127.0.0.1"
+;;         org-roam-server-port 9999
+;;         org-roam-server-authenticate nil
+;;         org-roam-server-export-inline-images t
+;;         org-roam-server-serve-files nil
+;;         org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+;;         org-roam-server-network-poll t
+;;         org-roam-server-network-arrows nil
+;;         org-roam-server-network-label-truncate t
+;;         org-roam-server-network-label-truncate-length 60
+;;         org-roam-server-network-label-wrap-length 20))
 
 (use-package htmlize
   :straight t
