ee2201d0ea0f23da50d43b7f8d6a63c48e603818
Nan Zhong <nzhong@digitalocean.com>
2021-08-19 09:51:24 -0400

    [emacs] Tweak avy keybinds

--- dotfiles/.config/emacs/core.el
+++ dotfiles/.config/emacs/core.el
@@ -206,11 +206,10 @@
 
 (use-package embark
   :straight t
-  :bind
-  (("C-." . embark-act)         ;; pick some comfortable binding
-   ("C-;" . embark-dwim)        ;; good alternative: M-.
-   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
-
+  :bind (:map minibuffer-local-map
+              ("M-." . embark-act)         ;; pick some comfortable binding
+              ("M-," . embark-dwim)        ;; good alternative: M-.
+              ("C-h B" . embark-bindings))
   :init
   ;; Optionally replace the key help with a completing-read interface
   (setq prefix-help-command #'embark-prefix-help-command)
