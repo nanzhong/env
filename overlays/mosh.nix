64181ffe3f5dbcb2feb4086371bb2253fa4b5493
Nan Zhong <nzhong@digitalocean.com>
2021-09-21 10:26:39 -0400

    [nix] Update mosh overlay to latest and remove aarch64-darwin patch

--- overlays/mosh.nix
+++ overlays/mosh.nix
@@ -5,14 +5,25 @@ self: super: {
     src = super.fetchFromGitHub {
       owner = "mobile-shell";
       repo = "mosh";
-      rev = "03087e7a761df300c2d8cd6e072890f8e1059dfa";
-      sha256 = "170m3q9sxw6nh8fvrf1l0hbx0rjjz5f5lzhd41143kd1rps3liw8";
+      rev = "e023e81c08897c95271b4f4f0726ec165bb6e1bd";
+      sha256 = "sha256-X2xJCiC5/vSijzZgQsWDzD+R8D8ppdZD6WeG4uoxyYw=";
     };
     patches = [
       "${super.path}/pkgs/tools/networking/mosh/ssh_path.patch"
       "${super.path}/pkgs/tools/networking/mosh/mosh-client_path.patch"
       "${super.path}/pkgs/tools/networking/mosh/utempter_path.patch"
+      # Removed backported w/c++17, ::bind vs std::bind patch since it's
+      # included in master branch.
       "${super.path}/pkgs/tools/networking/mosh/bash_completion_datadir.patch"
     ];
+
+    # Removed the aarch64-darwin patch in post patch since it's unused and
+    # causing conflicts with master branch.
+    postPatch = ''
+      substituteInPlace scripts/mosh.pl \
+          --subst-var-by ssh "${super.openssh}/bin/ssh"
+      substituteInPlace scripts/mosh.pl \
+          --subst-var-by mosh-client "$out/bin/mosh-client"
+    '';
   } );
 }
