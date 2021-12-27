9a27ddcca6d1230170181a95d688bd2bf9b93cf1
Nan Zhong <me@nanzho.ng>
2021-12-27 12:07:24 -0500

    [fish] Add greeting that checks for do calendar -> diary export

---
+++ dotfiles/.config/fish/functions/fish_greeting.fish
@@ -0,0 +1,18 @@
+function fish_greeting
+    if test -z "$init_do_calendar_diary"
+        if test ! -e "$HOME/.config/emacs/do.diary"
+            echo "[!] Work calendar export to emacs diary not configured."
+            echo "    1. Install gcal2diary:"
+            echo "       go install github.com/nanzhong/gcal2diary/cmd/gcal2diary@latest"
+            echo "    2. Configure client credentials:"
+            echo "       op get document 'gcal2diary Client Credentials' --output ~/.config/gcal2diary/credentials.json"
+            echo "    3. Setup cron on schedule"
+            echo "       */5 * * * * gcal2diary > ~/.config/emacs/do.diary"
+            echo
+            echo "    - To skip this reminder:"
+            echo "      set -U init_do_calendar_diary true"
+        else
+            set -U init_do_calendar_diary true
+        end
+    end
+end
