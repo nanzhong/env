68110eeca16fda512acd6e1f109b373d6ddace97
Nan Zhong <me@nanzho.ng>
2021-12-27 12:09:58 -0500

    [fish] Add org repo check to fish greeting

--- dotfiles/.config/fish/functions/fish_greeting.fish
+++ dotfiles/.config/fish/functions/fish_greeting.fish
@@ -1,4 +1,17 @@
 function fish_greeting
+    if test -z "$init_org"
+        if test ! -d "$HOME/org"
+            echo "[!] Org repo missing, some emacs configurations will not work."
+            echo "    1. Clone org repo:"
+            echo "       git clone git@github.com:nanzhong/org.git"
+            echo
+            echo "    - To skip this reminder:"
+            echo "      set -U init_org true"
+        else
+            set -U init_org true
+        end
+    end
+
     if test -z "$init_do_calendar_diary"
         if test ! -e "$HOME/.config/emacs/do.diary"
             echo "[!] Work calendar export to emacs diary not configured."
