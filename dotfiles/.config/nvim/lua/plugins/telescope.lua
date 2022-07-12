aa55155250a07574ea4fcbe2b6766c48169fbc69
Nan Zhong <me@nanzho.ng>
2022-07-11 20:14:24 -0400

    [nvim] Update telescope config to support hidden files and cd

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,23 @@ require('packer').use {
   'nvim-telescope/telescope.nvim',
   requires = { 'nvim-lua/plenary.nvim' },
   config = function ()
+    require('telescope').setup({
+      pickers = {
+        find_files = {
+          hidden = true,
+          mappings = {
+            n = {
+              ["cd"] = function(prompt_bufnr)
+                local selection = require("telescope.actions.state").get_selected_entry()
+                local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+                require("telescope.actions").close(prompt_bufnr)
+                vim.cmd(string.format("silent cd %s", dir))
+              end
+            }
+          }
+        }
+      }
+    })
     vim.keymap.set('n', '<Leader>ff', require('telescope.builtin').find_files)
     vim.keymap.set('n', '<Leader>fg', require('telescope.builtin').live_grep)
     vim.keymap.set('n', '<Leader>fb', require('telescope.builtin').buffers)
