9ae2b8d54853f416561c6da63382b15207bca8f7
Nan Zhong <me@nanzho.ng>
2022-07-13 19:52:35 -0400

    [nvim] Add dressing

---
+++ dotfiles/.config/nvim/lua/plugins/dressing.lua
@@ -0,0 +1,3 @@
+require('packer').use {
+  'stevearc/dressing.nvim'
+}
--- dotfiles/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/lspconfig.lua
@@ -9,6 +9,14 @@ require('packer').use {
 
     local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = 'rounded',
+    })
+
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+      border = 'rounded',
+    })
+
     require('lspconfig')['bashls'].setup({
       capabilities = capabilities,
     })
