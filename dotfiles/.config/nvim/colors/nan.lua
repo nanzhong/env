e4d30e6703c247a549510922ce0b863ab54f38cc
Nan Zhong <me@nanzho.ng>
2022-07-13 08:32:25 -0400

    [nvim] Revert to standard way on enabling lush

--- dotfiles/.config/nvim/colors/nan.lua
+++ dotfiles/.config/nvim/colors/nan.lua
@@ -1,6 +1,4 @@
-local lush = require('lush')
-
 vim.opt.background = 'dark'
 vim.g.colors_name = 'nan'
 package.loaded['lush_theme.nan'] = nil
-lush.apply(require('lush_theme.nan'))
+require('lush')(require('lush_theme.nan'))
