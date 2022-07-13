c3f1e1633c8490f32ef9d531452599b9f54946d6
Nan Zhong <nzhong@digitalocean.com>
2022-07-12 20:07:22 -0400

    [nvim] Customize lsp diag signs

--- dotfiles/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,11 @@
 require('packer').use {
   'neovim/nvim-lspconfig',
   config = function ()
+    vim.fn.sign_define('DiagnosticSignError', { texthl = 'DiagnosticSignError', text = '⚑' })
+    vim.fn.sign_define('DiagnosticSignWarn', { texthl = 'DiagnosticSignWarn', text = '⚐' })
+    vim.fn.sign_define('DiagnosticSignHint', { texthl = 'DiagnosticSignHint', text = '☻' })
+    vim.fn.sign_define('DiagnosticSignInfo', { texthl = 'DiagnosticSignInfo', text = '☺' })
+
     require('lspconfig')['bashls'].setup({})
     require('lspconfig')['cssls'].setup({})
     require('lspconfig')['gopls'].setup({})
