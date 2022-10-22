85c1e1f4509a83ffb43db6d50ddb373d2d1d9542
Nan Zhong <me@nanzho.ng>
2022-10-21 22:50:15 -0400

    [emacs] Switch from lsp-mode to eglot

--- dotfiles/.config/emacs/go.el
+++ dotfiles/.config/emacs/go.el
@@ -6,16 +6,12 @@
 
 (use-package go-mode
   :straight t
-  :after (lsp-mode)
   :mode "\\.go\\'"
   :hook (go-mode . (lambda ()
-                     (setq-local compile-command "go build -v && go test -race -cover -v && go vet")
-                     (setq-local compilation-read-command nil)
-                     (setq-local tab-width 2)
-                     (setq-local lsp-gopls-build-flags ["-tags=integration,sandbox"])
-                     (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\vendor\\'")
-                     (add-hook 'before-save-hook #'lsp-format-buffer nil t)
-                     (add-hook 'before-save-hook #'lsp-organize-imports nil t)))
+                     (setq compile-command "go build -v && go test -race -cover -v && go vet")
+                     (setq compilation-read-command nil)
+                     (setq tab-width 2)
+                     (add-hook 'before-save-hook #'eglot-format nil t)))
   :bind (:map go-mode-map
               ("C-c C-c" . compile)))
 
--- dotfiles/.config/emacs/lsp.el
+++ dotfiles/.config/emacs/lsp.el
@@ -3,45 +3,12 @@
 ;;; Commentary:
 
 ;;; Code:
-(use-package lsp-mode
-  :straight t
-  :after direnv
-  :commands lsp
-  :hook ((prog-mode . (lambda ()
-                        (direnv-update-environment)
-                        (lsp)))
-         (lsp-mode . (lambda ()
-                       (lsp-enable-which-key-integration)
-                       (lsp-modeline-diagnostics-mode)
-                       (lsp-modeline-code-actions-mode))))
-  :init
-  (setq lsp-keymap-prefix "C-l")
+(use-package eglot
+  :bind (:map eglot-mode-map
+              ("C-l r" . eglot-rename)
+              ("C-l f" . eglot-format)
+              ("C-l a" . eglot-code-actions))
   :config
-  (setq lsp-enable-indentation t
-        lsp-enable-on-type-formatting nil
-        lsp-enable-symbol-highlighting t
-        lsp-idle-delay 0.5
-        lsp-keep-workspace-alive nil
-        lsp-lens-debounce-interval 1.0
-        lsp-modeline-diagnostics-scope :file
-        ;; need to disable this until icon support for terminal emacs is fixed.
-        lsp-headerline-breadcrumb-enable nil
-        lsp-headerline-breadcrumb-icons-enable nil))
-
-(use-package lsp-ui
-  :straight t
-  :after lsp-mode
-  :commands lsp-ui-mode
-  :config
-  (setq lsp-ui-doc-header t
-        lsp-ui-doc-include-signature t
-        lsp-ui-doc-max-width 80
-        lsp-ui-doc-max-height 20
-        lsp-ui-doc-use-childframe nil))
-
-(use-package lsp-treemacs
-  :straight t
-  :after lsp-mode treemacs
-  :commands lsp-treemacs-errors-list)
+  (setq eglot-autoshutdown t))
 
 ;;; lsp.el ends here
