47d27118efeb2fb81e2bc528ff78f0ac4bf51dca
Nan Zhong <me@nanzho.ng>
2022-10-23 21:32:15 -0400

    [emacs] Enable eglot for go mode

--- dotfiles/.config/emacs/go.el
+++ dotfiles/.config/emacs/go.el
@@ -11,6 +11,7 @@
                      (setq compile-command "go build -v && go test -race -cover -v && go vet")
                      (setq compilation-read-command nil)
                      (setq tab-width 2)
+                     (eglot-ensure)
                      (add-hook 'before-save-hook #'eglot-format nil t)))
   :bind (:map go-mode-map
               ("C-c C-c" . compile)))
