4d6c91ccb25e9138479592075fe49f1cbdb716e0
Nan Zhong <me@nanzho.ng>
2022-11-24 12:11:27 -0500

    [emacs] Configure vue

--- dotfiles/.config/emacs/web.el
+++ dotfiles/.config/emacs/web.el
@@ -6,6 +6,8 @@
 
 (use-package web-mode
   :straight t
+  :hook (web-mode . (lambda ()
+                      (eglot-ensure)))
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
@@ -18,6 +20,11 @@
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs
+                 '(web-mode . ("vls" "--stdio")))))
 
 ;;; web.el ends here
