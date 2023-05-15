fd4e4a314c94468761de6c0d6f6875a17238d573
Nan Zhong <me@nanzho.ng>
2023-05-15 12:28:20 -0400

    [nix] Update homepi boot config

--- machines/homepi/configuration.nix
+++ machines/homepi/configuration.nix
@@ -8,6 +8,7 @@
   system.stateVersion = "22.05";
 
   boot = {
+    kernelPackages = pkgs.linuxPackages_rpi4;
     initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
     # ttyAMA0 is the serial console broken out to the GPIO
     kernelParams = [
@@ -24,7 +25,10 @@
         version = 4;
       };
       grub.enable = false;
+      generic-extlinux-compatible.enable = false;
     };
+
+    tmp.useTmpfs = true;
   };
 
   nanzhong = {
