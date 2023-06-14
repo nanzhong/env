647f9a7f9d991e837ab7a554dc13ad176280e05d
Nan Zhong <me@nanzho.ng>
2023-06-14 00:23:48 -0400

    [fish] Fix incorrect nix path ordering in fish

--- modules-darwin/common/default.nix
+++ modules-darwin/common/default.nix
@@ -4,5 +4,8 @@ with lib;
   imports = [ ../../modules/common/default.nix ];
   config = {
     homebrew.enable = true;
+
+    # This fixes the inpcorrect path ordering
+    programs.fish.loginShellInit = ''for p in (string split " " $NIX_PROFILES); fish_add_path --prepend --move $p/bin; end'';
   };
 }
