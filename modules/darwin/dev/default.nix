0969138d3a72c92cb7e8cbc5b2e911ac0fed07df
Nan Zhong <me@nanzho.ng>
2023-06-18 20:22:39 -0400

    [nix] Properly replace all icons

--- modules/darwin/dev/default.nix
+++ modules/darwin/dev/default.nix
@@ -7,8 +7,20 @@ let
     withGTK2 = false;
     withGTK3 = false;
   }).overrideAttrs (old: {
-    patches = old.patches ++ [
-#      ../../patches/emacs/frame-refocus.patch
+    postPatch = concatStringsSep "\n" [
+      old.postPatch
+      ''
+        echo "Patching emacs icon..."
+        cp "${../../../patches/emacs/Emacs.icns}" "nextstep/Cocoa/Emacs.base/Contents/Resources/Emacs.icns"
+        cp "${../../../patches/emacs/emacs.tiff}" "nextstep/GNUstep/Emacs.base/Resources/emacs.tiff"
+        cp "${../../../patches/emacs/emacs.svg}" "etc/images/icons/hicolor/scalable/apps/emacs.svg"
+        cp "${../../../patches/emacs/emacs.ico}" "etc/images/icons/hicolor/scalable/apps/emacs.ico"
+        cp "${../../../patches/emacs/emacs-128.png}" "etc/images/icons/hicolor/128x128/apps/emacs.png"
+        cp "${../../../patches/emacs/emacs-48.png}" "etc/images/icons/hicolor/48x48/apps/emacs.png"
+        cp "${../../../patches/emacs/emacs-32.png}" "etc/images/icons/hicolor/32x32/apps/emacs.png"
+        cp "${../../../patches/emacs/emacs-24.png}" "etc/images/icons/hicolor/24x24/apps/emacs.png"
+        cp "${../../../patches/emacs/emacs-16.png}" "etc/images/icons/hicolor/16x16/apps/emacs.png"
+      ''
     ];
   });
 in
