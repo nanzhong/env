80bfc9c77729d5897c5dbcf0b01cb581c9008f91
Nan Zhong <me@nanzho.ng>
2023-07-05 21:17:30 -0400

    [nix] Update alacritty overlay to v0.12.2

--- overlays/alacritty.nix
+++ overlays/alacritty.nix
@@ -1,11 +1,11 @@
 self: super: {
   alacritty = super.alacritty.overrideAttrs (old: rec {
-    version = "nanzhong-master";
+    version = "nanzhong-patched-macos-icon";
     src = super.fetchFromGitHub {
       owner = "nanzhong";
       repo = old.pname;
       rev = "refs/heads/patched-macos-icon";
-      hash = "sha256-zwXEdNY6mHhzukGlveZjdVWMbVSsNVZrs23uy3uSjvM=";
+      hash = "sha256-CO7cZ3ljdM/Bd5fcr23SfEm/GeGa5ftk+1kNO2MAj/I=";
     };
 
     checkFlags = [
