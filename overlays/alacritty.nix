9a2b31c9b816b2c233e329c9262f431dfd3eaa58
Nan Zhong <me@nanzho.ng>
2023-10-24 11:26:37 -0400

    [nix] Update alacritty

--- overlays/alacritty.nix
+++ overlays/alacritty.nix
@@ -5,7 +5,7 @@ self: super: {
       owner = "nanzhong";
       repo = old.pname;
       rev = "refs/heads/patched-macos-icon";
-      hash = "sha256-CO7cZ3ljdM/Bd5fcr23SfEm/GeGa5ftk+1kNO2MAj/I=";
+      hash = "sha256-IfQFmDvl1zkvSTqK4hYOURfpB5gKbqt58A5MfmksNZ0=";
     };
 
     checkFlags = [
