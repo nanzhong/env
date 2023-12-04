0005ea986181555d33487ffec2b2626610068cf0
Nan Zhong <me@nanzho.ng>
2023-12-04 10:29:32 -0500

    [fzf] Update colours

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -7,7 +7,7 @@ if status is-interactive
   set -gx EDITOR 'hx'
   set -gx VISUAL 'hx'
   set -gx FZF_DEFAULT_COMMAND 'fd --hidden .'
-  set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+  set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --color="bg+:#1e2529,border:#272f35,gutter:#1e2529"'
   set -gx FORGIT_FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS
   set -gx TERMINFO_DIRS $HOME/.terminfo $TERMINFO_DIRS
 end
