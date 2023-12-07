0c6c741c63858211d6fbe46c04858802958005d2
Nan Zhong <me@nanzho.ng>
2023-12-06 23:44:50 -0500

    [neovim] Update lsp config

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -22,46 +22,50 @@ return {
       end
 
       local opts = { noremap = true, silent = true }
-      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, with_desc(opts, 'Open diagnostic float'))
-      vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, with_desc(opts, 'Goto prev diagnostic'))
-      vim.keymap.set('n', ']e', vim.diagnostic.goto_next, with_desc(opts, 'Goto next diagnostic'))
+      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, with_desc(opts, 'Open diagnostic float'))
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, with_desc(opts, 'Goto prev diagnostic'))
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, with_desc(opts, 'Goto next diagnostic'))
       vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, with_desc(opts, 'Open diagnastic loclist'))
 
-      local on_attach = function(_, bufnr)
-        -- Mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, with_desc(bufopts, 'Goto declaration'))
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, with_desc(bufopts, 'Goto definition'))
-        vim.keymap.set('n', 'gdd', vim.lsp.buf.type_definition, with_desc(bufopts, 'Goto type definition'))
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, with_desc(bufopts, 'Goto implementation'))
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, with_desc(bufopts, 'Goto references'))
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, with_desc(bufopts, 'Show documentation'))
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, with_desc(bufopts, 'Show signature help'))
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, with_desc(bufopts, 'Add workspace folder'))
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, with_desc(bufopts, 'Remove workspace folder'))
-        vim.keymap.set('n', '<leader>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, with_desc(bufopts, 'List workspace folders'))
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, with_desc(bufopts, 'Rename'))
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, with_desc(bufopts, 'Code actions'))
-        vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, with_desc(bufopts, 'Format'))
-      end
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local bufopts = { noremap = true, silent = true, buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, with_desc(bufopts, 'Goto declaration'))
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, with_desc(bufopts, 'Goto definition'))
+          vim.keymap.set('n', 'gdd', vim.lsp.buf.type_definition, with_desc(bufopts, 'Goto type definition'))
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, with_desc(bufopts, 'Goto implementation'))
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, with_desc(bufopts, 'Goto references'))
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, with_desc(bufopts, 'Show documentation'))
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, with_desc(bufopts, 'Show signature help'))
+          vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, with_desc(bufopts, 'Add workspace folder'))
+          vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, with_desc(bufopts, 'Remove workspace folder'))
+          vim.keymap.set('n', '<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, with_desc(bufopts, 'List workspace folders'))
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, with_desc(bufopts, 'Rename'))
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, with_desc(bufopts, 'Code actions'))
+          vim.keymap.set('n', '<leader>f', function()
+            vim.lsp.buf.format { async = true }
+          end, with_desc(bufopts, 'Format'))
+        end,
+      })
 
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require('lspconfig')
       lspconfig.bashls.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
       }
       lspconfig.cssls.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
       }
       lspconfig.gopls.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
         settings = {
           gopls = {
             buildFlags = {'-tags=integration'}
@@ -70,19 +74,15 @@ return {
       }
       lspconfig.html.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
       }
       lspconfig.jsonls.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
       }
       lspconfig.rust_analyzer.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
       }
       lspconfig.sqlls.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
       }
       lspconfig.lua_ls.setup {
         on_init = function(client)
@@ -116,14 +116,15 @@ return {
       }
       lspconfig.tsserver.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
       }
       lspconfig.volar.setup {
         capabilities = capabilities,
-        on_attach = on_attach,
         filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
       }
-      lspconfig.zig.setup {}
+      lspconfig.zig.setup {
+        capabilities = capabilities,
+        filetyes = { "zig", "zir", "zon" }
+      }
     end
   }
 }
