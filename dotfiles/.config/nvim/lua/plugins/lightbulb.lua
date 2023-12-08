63c4b3e7ea97bb73ea6bf9f8a5409b8c836807eb
Nan Zhong <me@nanzho.ng>
2023-12-07 19:46:43 -0500

    [neovim] Update lightbulb config

--- dotfiles/.config/nvim/lua/plugins/lightbulb.lua
+++ dotfiles/.config/nvim/lua/plugins/lightbulb.lua
@@ -1,13 +1,13 @@
 return {
   {
     'kosayoda/nvim-lightbulb',
-    dependencies = 'antoinemadec/FixCursorHold.nvim',
     config = function()
-      vim.fn.sign_define('LightBulbSign', { text = '⨀', texthl = 'LightBulbSign' })
-  
       require('nvim-lightbulb').setup({
         autocmd = {
           enabled = true
+        },
+        sign = {
+          text = '⨀',
         }
       })
     end
