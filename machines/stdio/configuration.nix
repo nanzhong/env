b5abc89b4cd8f09c85dbfceb6f210a42e6ad4546
Nan Zhong <me@nanzho.ng>
2024-01-09 11:58:02 -0500

    [nix] Remove alacritty

--- machines/stdio/configuration.nix
+++ machines/stdio/configuration.nix
@@ -12,16 +12,11 @@ with lib;
 
   nixpkgs = {
     overlays = [
-      (import ../../overlays/alacritty.nix)
       (import ../../overlays/fzf-fish.nix)
     ];
   };
 
   environment.shells = [ pkgs.fish ];
-  environment.systemPackages = with pkgs; [
-    alacritty
-  ];
-
   services.tailscale.enable = true;
   system.checks.verifyBuildUsers = false;
 }
