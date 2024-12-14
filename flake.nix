53c21b1b779f2e696db11d124a19ee7b85a07a92
Nan Zhong <me@nanzho.ng>
2024-12-14 15:30:55 -0500

    [nix] Set trusted substitutors and public keys

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "Nan's environment configurations";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
--- modules/common/default.nix
+++ modules/common/default.nix
@@ -11,6 +11,17 @@ with lib;
 
       package = pkgs.nixVersions.latest;
 
+      settings = {
+        substituters = [
+          "https://cache.nixos.org/"
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+         ];
+      };
+
       gc = {
         automatic = true;
         options = "--delete-older-than 30d";
