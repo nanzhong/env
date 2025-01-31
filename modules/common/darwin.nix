a1b83d71ac7aca2ec60262a435b9bfc12db97efa
Nan Zhong <me@nanzho.ng>
2025-01-31 09:58:55 -0500

    [nix] Cleanup trampoline

--- modules/common/darwin.nix
+++ modules/common/darwin.nix
@@ -3,15 +3,6 @@ with lib;
 {
   imports = [ ./default.nix ];
   config = {
-    # This is a workaround to setup finder aliases for gui applications so that spotlight can find them
-    system.activationScripts.postUserActivation.text = ''
-      apps_source="${config.system.build.applications}/Applications"
-      moniker="Nix Trampolines"
-      app_target_base="$HOME/Applications"
-      app_target="$app_target_base/$moniker"
-      mkdir -p "$app_target"
-      ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
-    '';
     nix.enable = false;
   };
 }
